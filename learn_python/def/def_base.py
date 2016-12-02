ac337d372bcece1db3cc05306efed452b7dad1f6
gushaofeng <gushaofeng@qhfax.com.cn>
2016-12-02 09:19:45 +0800

    add learn_python

---
+++ learn_python/def/def_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#=============================================================================
+#     FileName:
+#         Desc:
+#       Author: 顾少
+#        Email: vekergu@163.com
+#     HomePage: https://github.com/vekergu
+#      Version: 0.0.1
+#   LastChange: 
+#      History:
+#=============================================================================
